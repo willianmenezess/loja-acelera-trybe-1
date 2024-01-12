deac649c04ee7137ad127b46643315cb4250727c
willianmenezess <willmenezes@outlook.com.br>
2024-01-12 16:32:29 -0300

    feat: comentários explicativos

--- backend/src/services/transaction.service.js
+++ backend/src/services/transaction.service.js
@@ -15,6 +15,8 @@ const getAll = async ({ accountId, status = true }) => {
 
 const createCashback = async ({ transactionId, cashback }) => {
   const transaction = await model.createCashback({ transactionId, cashback });
+  // o httpException é um middleware que vai tratar o erro e poderia estar 
+  // na camada controller
   if (!transaction) throw new HttpException(400, 'Nothing changed');
   return transaction;
 };
